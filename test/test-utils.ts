5e742828c35410f1d21d799ce917131bae955abe
whxaxes <whxaxes@gmail.com>
2022-12-12 16:45:03 +0800

    fix: update cwd

--- test/test-utils.ts
+++ test/test-utils.ts
@@ -6,12 +6,12 @@ import { CommandTrigger } from '../src/core/trigger';
 import { ForkOptions } from 'child_process';
 
 export function fork(target: string, args: string[] = [], options: ForkOptions = {}) {
-  const env = { ...(options.env || process.env), TS_NODE_PROJECT: path.resolve(__dirname, './tsconfig.json') };
+  const env = { ...(options.env || process.env) };
   // or use coffee.beforeScript to register ts-node
   // TODO: refactor to clet
   const bin = path.join(__dirname, 'fixtures', target, 'bin/cli.ts');
   return coffee.fork(bin, args, {
-    cwd: __dirname,
+    cwd: path.resolve(__dirname, '../'), // make sure TS_NODE_PROJECT is right
     execArgv: [ '-r', 'ts-node/register' ].concat(options.execArgv || []),
     ...options,
     env,
