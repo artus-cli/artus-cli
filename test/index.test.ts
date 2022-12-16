819babf77358f42ae5b99a19ba98bb80eb6d4b23
whxaxes <whxaxes@gmail.com>
2022-12-16 18:25:46 +0800

    fix: ci

--- test/index.test.ts
+++ test/index.test.ts
@@ -118,7 +118,7 @@ describe('test/index.test.ts', () => {
 
     await fork('chair-bin', [ 'oneapi' ])
       .debug()
-      .expect('stdout', /Command not found: 'chair-bin oneapi'/)
+      .expect('stderr', /Command not found: 'chair-bin oneapi'/)
       .end();
 
     await fork('chair-bin', [ 'user', '-u=123' ])
