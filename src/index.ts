e31d8d295e668c0a71cbb9c3f64c60cabce2338d
whxaxes <whxaxes@gmail.com>
2022-12-19 12:54:01 +0800

    fix: remove unused import

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import 'reflect-metadata';
 import { ArtusApplication, Scanner } from '@artus/core';
 import { ApplicationOptions } from './types';
 import assert from 'node:assert';
-import fs from 'node:fs';
 import { readPkg, getCalleeDir } from './utils';
 import path from 'node:path';
 
